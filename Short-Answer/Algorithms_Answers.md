27cad3f744f71bc7801d6145b993da5e0689c224
Jing Qian <qianjing2020@users.noreply.github.com>
2020-07-24 13:45:30 -0400

    initial submission, all done except stretch

--- Short-Answer/Algorithms_Answers.md
+++ Short-Answer/Algorithms_Answers.md
@@ -2,14 +2,27 @@
 
 ## Exercise I
 
-a)
+a) O(n^3)
 
 
-b)
+b) O(n*logn)
 
 
-c)
+c) O(2^n)
 
 ## Exercise II
 
+Problem:
+We don't know which floor is safe to drop eggs so we have lots of eggs to test out. Drop eggs from different floors of a n-story building, if floor <= f, egg safe, other wise egg broken. Find f, and minimize the egg use. 
 
+Algorithm/psuedocode:
+
+Set initial condition: f = 0 (ground floor), egg_broken = 0 (false)
+Start from ground floor, drop egg
+If egg_broken is true: 
+    return 0
+While egg_broken is false AND f <= n
+    f = f+1 
+return f
+
+Runtime is O(1) because my algorithm starts from floor 0, rather than floor n. And since f depends on egg physics rather than n, finding f would take a constant time!
--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -3,8 +3,21 @@ Your function should take in a single parameter (a string `word`)
 Your function should return a count of how many occurences of ***"th"*** occur within `word`. Case matters.
 Your function must utilize recursion. It cannot contain any loops.
 '''
-def count_th(word):
-    
-    # TBC
-    
-    pass
+def count_th(word):    
+    # base case no th in word
+    search = 'th'
+    len1 = len(search)
+    len2 = len(word)
+    # special case: word = []
+    if len2 == 0:
+        return 0
+    # base case: word has less letter than th:
+    if len2 < len1:
+        return 0
+    # recursve case, start from first len1 in word
+    # if search match first two character, add 1 
+    if search == word[0:len1]: 
+        return 1 + count_th(word[1:])
+    # otherwise, search the right by jump to the next character    
+    else:
+        return count_th(word[1:])   
